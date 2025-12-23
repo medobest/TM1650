035de13b1adfceee7926519892e8fed3d3dbd380
medobest <93018289+medobest@users.noreply.github.com>
2025-12-24 01:17:37 +0200

    Update TM1650_example01.ino

--- examples/TM1650_example01/TM1650_example01.ino
+++ examples/TM1650_example01/TM1650_example01.ino
@@ -4,10 +4,38 @@
 #define greendot 0
 #define semicol 1
 TM1650 d;
-unsigned long previousMillis = 0;  // will store last time LED was updated
 
-// constants won't change:
-const long interval = 300;  // interval at which to blink (milliseconds)
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 300;         // interval at which to blink (milliseconds)
+
+bool st = true;
+
+//int ignoredKeys[] = {15, 47, 23, 55, 31, 7, 39};
+//int arraySize = sizeof(targetKeys) / sizeof(targetKeys[0]);
+
+void Turndevice() {
+  if (st) {
+
+
+
+    char line[] = "on  ";
+    d.displayString(line);
+    d.setDot(reddot, true);
+    d.setDot(greendot, true);
+
+
+  } else {
+
+    char line[] = "off ";
+    d.displayString(line);
+    d.setDot(reddot, true);
+    d.setDot(greendot, false);
+  }
+}
+
+
+
+
 void setup() {
   Wire.begin();  //Join the bus as master
 
@@ -16,14 +44,12 @@ void setup() {
   d.init();
   d.displayOff();
   d.displayOn();
-  d.displayString("____");
   d.setBrightness(TM1650_MAX_BRIGHT);
-  delay(100);
-  char line[] = "on  ";
-  d.displayString(line);
-  d.setDot(reddot, true);
+  d.displayString("____");
+  st = true;
+  Turndevice();
 }
-
+uint8_t oldkey = 0;
 void loop() {
 
 
@@ -32,14 +58,14 @@ void loop() {
     previousMillis = currentMillis;
     uint8_t key = d.readButtons();
 
-    if (key != 0) {
-
+    if (key != oldkey) {
       switch (key) {
-
         case 79:
           // statements
           Serial.print("Button Data Received: ");
           Serial.println("Power");
+          st = !st;
+          Turndevice();
           break;
         case 111:
           // statements
@@ -72,8 +98,7 @@ void loop() {
           Serial.println("OK");
           break;
       }
-
-      delay(200);
+      oldkey = key;
     }
   }
 }
